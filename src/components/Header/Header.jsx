a81449d57b96de959758b59ed3e9c02f22d8a289
Rudra Roy <royrudra164@gmail.com>
2024-03-29 04:04:45 +0530

    home page updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -47,13 +47,13 @@ function Header() {
 
               </Link>
           </div>
-          <ul className='flex ml-auto'>
+          <ul className='flex ml-auto gap-2'>
             {navItems.map((item) => 
             item.active ? (
               <li key={item.name}>
                 <button
                 onClick={() => navigate(item.slug)}
-                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                className='inline-bock px-6 py-2 duration-200 bg-blue-700 hover:bg-blue-500 rounded-full text-white text-sm'
                 >{item.name}</button>
               </li>
             ) : null
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,63 +1,88 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 import appwriteService from "../appwrite/config";
-import {Container, PostCard} from '../components'
-import { useSelector } from 'react-redux';
+import { Container, PostCard } from "../components";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+// import {Blog} from "../assets/blog"
 
 function Home() {
-    const [posts, setPosts] = useState([])
-    const authStatus = useSelector((state) => state.auth.status)
+  const [posts, setPosts] = useState([]);
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        appwriteService.getPosts().then((posts) => {
-            if (posts) {
-                setPosts(posts.documents)
-            }
-        })
-    }, [])
-  
-    if (!authStatus) {
-        return (
-            <div className="w-full py-8 mt-4 text-center">
-                <Container>
-                    <div className="flex flex-wrap">
+  useEffect(() => {
+    appwriteService.getPosts().then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  }, []);
+
+  if (!authStatus) {
+    return (
+      <div className="w-full py-8 mt-4">
+        <Container>
+          {/* <div className="flex flex-wrap">
                         <div className="p-2 w-full">
                             <h1 className="text-2xl font-bold hover:text-gray-500">
                                 please login
                             </h1>
                         </div>
-                    </div>
-                </Container>
+                    </div> */}
+          <div className="flex m-5 flex-wrap gap-x-20 gap-y-10 justify-center">
+            <div className="">
+              <h1 className="text-4xl ">WELCOME TO YOUR BLOG APP</h1>
+              <div className="mt-5 flex gap-4 justify-center">
+                <button
+                  className="bg-black text-white px-4 py-2 rounded-lg"
+                  onClick={() => navigate("/login")}
+                >
+                  Login
+                </button>
+                <button
+                  className="bg-black text-white px-4 py-2 rounded-lg"
+                  onClick={() => navigate("/signup")}
+                >
+                  Signup
+                </button>
+              </div>
             </div>
-        )
-    }
-    else if (authStatus && posts.length === 0) {
-        return (
-            <div className="w-full py-8 mt-4 text-center">
-                <Container>
-                    <div className="flex flex-wrap">
-                        <div className="p-2 w-full">
-                            <h1 className="text-2xl font-bold hover:text-gray-500">
-                                no post
-                            </h1>
-                        </div>
-                    </div>
-                </Container>
-            </div>
-        )
-    }
+            {/* <div className="flex flex-grow lg:justify-center ">
+             <img src="https://i.ibb.co/6b6Bx0t/1.png" alt="" />
+            </div> */}
+          </div>
+          <div></div>
+        </Container>
+      </div>
+    );
+  } else if (authStatus && posts.length === 0) {
     return (
-        <div className='w-full py-8'>
-            <Container>
-                <div className='flex flex-wrap'>
-                    {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
-                            <PostCard {...post} />
-                        </div>
-                    ))}
-                </div>
-            </Container>
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+              <h1 className="text-2xl font-bold hover:text-gray-500">
+                no post
+              </h1>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+  return (
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap">
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-1/4">
+              <PostCard {...post} />
+            </div>
+          ))}
         </div>
-    )
+      </Container>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
